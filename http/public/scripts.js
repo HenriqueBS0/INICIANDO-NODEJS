190f17dcaee9fc94bdbbc8fee15a16e9ec10a1b8
Henrique Borges dos Santos <henrique.10agr@gmail.com>
2021-05-01 16:42:47 -0300

    frontend/api integration

--- http/public/scripts.js
+++ http/public/scripts.js
@@ -2,6 +2,21 @@ const ul = document.querySelector("ul")
 const input = document.querySelector("input")
 const form = document.querySelector('form')
 
+async function load() {
+    const res = await fetch("http://localhost:3000").then(data => data.json())
+    res.urls.map(url => addElement(url))
+}
+
+load();
+
+async function deleteLink({ name, url }) {
+    fetch(`http://localhost:3000/?name=${name}&url=${url}&del=1`);
+}
+
+async function addLink({ name, url }) {
+    fetch(`http://localhost:3000/?name=${name}&url=${url}`);
+}
+
 
 function addElement({ name, url }) {
     const li = document.createElement('li')
@@ -21,8 +36,16 @@ function addElement({ name, url }) {
 }
 
 function removeElement(el) {
-    if (confirm('Tem certeza que deseja deletar?'))
+    if (confirm('Tem certeza que deseja deletar?')) {
+        const linkElement = el.parentNode.querySelector("a");
+        const link = {
+            name: linkElement.innerHTML,
+            url: linkElement.href.replace(/\/$/g, '')
+        }
+        deleteLink(link);
         el.parentNode.remove()
+    }
+
 }
 
 form.addEventListener("submit", (event) => {
@@ -41,6 +64,7 @@ form.addEventListener("submit", (event) => {
     if (!/^http/.test(url))
         return alert("Digite a url da maneira correta")
 
+    addLink({ name, url })
     addElement({ name, url })
 
     input.value = ""
